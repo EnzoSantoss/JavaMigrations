3896bd4d5d9d6880a27d03d6c5ae77652ccab232
EnzoSantoss <enzoaugusto312@gmail.com>
2023-07-26 20:22:37 -0300

    first commit

---
+++ src/main/java/com/migrationsComJava/javaMigrations/JavaMigrationsApplication.java
@@ -0,0 +1,13 @@
+package com.migrationsComJava.javaMigrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMigrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMigrationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migrationsComJava/javaMigrations/JavaMigrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migrationsComJava.javaMigrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMigrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
